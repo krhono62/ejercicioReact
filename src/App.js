86b9a5aea0cd0d548d3874f3a37816dc2119dcda
Miguel <krhono62@gmail.com>
2021-09-13 13:08:06 -0500

    Hicimos las tablas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './componentes/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Ingresa el numero para obtener su tabla:</h1>
+      <input id="numero" placeholder="Ejemplo 9"/>
+      <button class="button default" onClick={generarTabla}>Hola</button>
+      <div id="resultado">
+
+        
+      </div>
       </header>
     </div>
   );
 }
+
+function generarTabla(){
+  var j= document.getElementById('numero').value;
+
+	//i++ significa i=i+1;
+
+	for(var i = 1;i<=10;i++){
+		document.write("<li>");
+		document.write(j + "x " + i + "= " + j * i);
+		document.write("</li>");
+	}
+
+	document.write("</ul>");
+}
+
 
 export default App;
